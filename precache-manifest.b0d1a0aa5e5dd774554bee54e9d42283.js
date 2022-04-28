8c8edf4a6df057b23da83ec970fe0415223493db
Marek Stefanowski <maza@Mareks-MacBook-Pro-4.local>
2022-04-28 22:21:58 +0100

    Updates

---
+++ precache-manifest.b0d1a0aa5e5dd774554bee54e9d42283.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "06ac8d2e0b6de37f4b09",
+    "url": "/robofriends/static/css/main.2c0b4540.chunk.css"
+  },
+  {
+    "revision": "06ac8d2e0b6de37f4b09",
+    "url": "/robofriends/static/js/main.06ac8d2e.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "38a7b11ef43c7ee56ac9",
+    "url": "/robofriends/static/css/2.4dd5c582.chunk.css"
+  },
+  {
+    "revision": "38a7b11ef43c7ee56ac9",
+    "url": "/robofriends/static/js/2.38a7b11e.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "6cf50c43d0fc3da5d4a491d6057aaed6",
+    "url": "/robofriends/index.html"
+  }
+];
